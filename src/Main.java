be11607cb4500f5ea9da34b2901e053268f33dbd
Андрей <andrews.ru83@gmail.com>
2022-06-29 21:24:57 +0300

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> words = new ArrayList<>(List.of(
+                "если", "бы", "солнце", "восходило",
+                "на", "западе", "солнце",
+                "было", "бы", "реверсивным"
+        ));
 
-        System.out.println("Hello world!");
+        Map<String, Integer> result = new HashMap<>();
+        Set <String> unique = new HashSet<>(words) ;
+        for (String string: unique){
+
+            if (Collections.frequency(words, string) != 1) {
+                result.put(string, Collections.frequency(words, string)) ;
+            }
+            }
+        
+        System.out.println(result);
+        }
     }
-}
