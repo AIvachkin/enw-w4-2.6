f6d98b7c1fabe5289870b4af5ff6c0025a11b71f
Андрей <andrews.ru83@gmail.com>
2022-06-29 20:22:08 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        List <String> words = new ArrayList<>(List.of(
+                "вода",
+                "картофель",
+                "серена",
+                "свора",
+                "корабль",
+                "манекен",
+                "корабль",
+                "радуга",
+                "вода"
+        ));
+
+       Set<String> uniqueWords = new HashSet<>(words) ;
+        System.out.println(uniqueWords);
     }
 }
