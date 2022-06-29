05a93dfdc07ffe7c40ac414867a6383ac6af0d63
Андрей <andrews.ru83@gmail.com>
2022-06-29 21:35:46 +0300

    Задание 2 + разбор

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.*;
 
 public class Main {
+
+    static List<Integer> nums = new ArrayList<>(List.of(1, 2, 8, 14, 1, 5, 8, 2, 3, 4, 1, 4, 5, 5, 6, 7));
+
     public static void main(String[] args) {
 
-        List<Integer> nums = new ArrayList<>(List.of(1, 2, 8, 14, 1, 5, 8, 2, 3, 4, 1, 4, 5, 5, 6, 7));
         Set<Integer> set = new HashSet<>();
         set.addAll(nums);
         List<Integer> numsTemp = new ArrayList<>();
@@ -14,6 +16,18 @@ public class Main {
                 System.out.println(integer);
             }
 
+        }
+        task2 ();
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println(num);
+                prevNum = num ;
+            }
         }
     }
 }
