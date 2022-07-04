9c6dd3b35ddbad07600deb7b0064e324dfd22ac6
Андрей <andrews.ru83@gmail.com>
2022-07-04 21:00:01 +0300

    Задание 4 + разбор + доп

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,33 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
+
+    static List<Integer> nums = new ArrayList<>(List.of(1, 2, 8, 14, 1, 5, 8, 2, 3, 4, 1, 4, 5, 5, 6, 7));
+
     public static void main(String[] args) {
 
-        List <String> words = new ArrayList<>(List.of(
-                "вода",
-                "картофель",
-                "серена",
-                "свора",
-                "корабль",
-                "манекен",
-                "корабль",
-                "радуга",
-                "вода"
-        ));
+        Set<Integer> set = new HashSet<>();
+        set.addAll(nums);
+        List<Integer> numsTemp = new ArrayList<>();
+        numsTemp.addAll(set);
+        Collections.sort(numsTemp);
+        for (Integer integer : numsTemp) {
+            if (integer % 2 == 0) {
+                System.out.println(integer);
+            }
 
-       Set<String> uniqueWords = new HashSet<>(words) ;
-        System.out.println(uniqueWords);
+        }
+        task2 ();
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println(num);
+                prevNum = num ;
+            }
+        }
     }
 }
