66f8443208d4a8e8dba6b3dcc95f0d5cba58c47d
Андрей <andrews.ru83@gmail.com>
2022-07-04 20:44:04 +0300

    Задание 4 + разбор + доп

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        doTask1();
+    }
 
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7,0,9)) ;
+    private static void doTask1() {
+                List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7,0,9)) ;
 
         for (Integer num : nums) {
             if (num % 2 != 0) {
